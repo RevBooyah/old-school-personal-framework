ee1fea6045d7b8fdb595b5b922c6e02a8c2dae9a
Steve Cook <booyahmedia@gmail.com>
2013-07-22 16:56:34 -0600

    Basic HTML5 template added.

---
+++ template/footer.php
@@ -0,0 +1,49 @@
+
+	
+<!-- FOOTER BEGINS -->
+
+
+<!-- If wanted, you can move aside content to a different include -->
+	<aside>
+	
+		<h2>Sidebar Content</h2>
+	
+	</aside>
+	
+	<footer>
+		
+		<p><small>&copy; Copyright 2010-<?php echo date("Y"); ?> <?php echo DOMAIN.".com";?>. All Rights Reserved.</small></p>
+		
+	</footer>
+
+</div>
+
+<!-- Grab Google CDN's jQuery. fall back to local if necessary -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write("<script src='_/js/jquery-1.9.1.min.js'>\x3C/script>")</script>
+
+<!-- this is where we put our custom functions -->
+<!-- don't forget to concatenate and minify if needed -->
+<script src="/js/functions.js"></script>
+
+<!-- Asynchronous google analytics; this is the official snippet.
+	 Replace UA-XXXXXX-XX with your site's ID and uncomment to enable.
+	 
+<script>
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-XXXXXX-XX']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+-->
+  
+</body>
+</html>
+
